181ec221b4423f274f5e929d9c7e864b11034f19
Renaud BOYER <rboyer@linagora.com>
2020-11-16 21:39:15 +0100

    Add components and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ContactsComponent } from './contacts.component';
+import { ContactComponent } from './contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
